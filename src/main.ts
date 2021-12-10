dd3bcb79adaca1aaa05c4e148403808da13cf436
dprastha <danielprastha4@gmail.com>
2021-12-10 09:25:37 +0700

    add transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('HTTP_PORT');
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(port);
