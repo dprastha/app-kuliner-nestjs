94b7f297008922cdb87b23e0c3788f90deab33db
dprastha <danielprastha4@gmail.com>
2021-12-10 09:23:56 +0700

    add transform interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
   const logger = new Logger();
@@ -10,6 +11,7 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   const port = configService.get('HTTP_PORT');
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
