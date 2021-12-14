c6d208d945ef5afd130dd5fd96e508db8269c0d8
dprastha <danielprastha4@gmail.com>
2021-12-14 10:02:32 +0700

    add foods filter

--- src/foods/foods.repository.ts
+++ src/foods/foods.repository.ts
@@ -3,14 +3,26 @@ import { EntityRepository, Repository } from 'typeorm';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { Food } from '../entities/food.entity';
 import { Logger } from '@nestjs/common';
+import { GetFoodsFilterDto } from './dto/get-foods-filter.dto';
 @EntityRepository(Food)
 export class FoodsRepository extends Repository<Food> {
   private logger = new Logger('Food Repository', { timestamp: true });
-  async getFoods(): Promise<Food[]> {
+  async getFoods(filterDto: GetFoodsFilterDto): Promise<Food[]> {
+    const { search } = filterDto;
+    const query = this.createQueryBuilder('food').leftJoinAndSelect(
+      'food.origin',
+      'origin',
+    );
+
+    if (search) {
+      query.andWhere(
+        '(LOWER(food.name) LIKE LOWER(:search) OR LOWER(food.description) LIKE LOWER(:search))',
+        { search: `%${search}%` },
+      );
+    }
+
     try {
-      const foods = this.find({
-        relations: ['origin'],
-      });
+      const foods = query.getMany();
       return foods;
     } catch (error) {
       this.logger.error(`Failed retrieving all food data`), error.stack;
